a76e3ff10f2f809ead09d9f35cc7d9cfcff55f69
Andreas Veithen <andreas.veithen@gmail.com>
2018-10-27 11:37:22 +0100

    Initial commit

---
+++ src/main/java/com/github/veithen/maven/wsimport/AbstractGenerateMojo.java
@@ -0,0 +1,123 @@
+/*-
+ * #%L
+ * wsimport-maven-plugin
+ * %%
+ * Copyright (C) 2018 Andreas Veithen
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.github.veithen.maven.wsimport;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.xml.sax.SAXParseException;
+
+import com.sun.codemodel.CodeWriter;
+import com.sun.codemodel.writer.FileCodeWriter;
+import com.sun.tools.ws.processor.generator.SeiGenerator;
+import com.sun.tools.ws.processor.generator.ServiceGenerator;
+import com.sun.tools.ws.processor.model.Model;
+import com.sun.tools.ws.processor.modeler.wsdl.WSDLModeler;
+import com.sun.tools.ws.wscompile.ErrorReceiver;
+import com.sun.tools.ws.wscompile.WsimportOptions;
+import com.sun.tools.ws.wsdl.parser.MetadataFinder;
+import com.sun.tools.ws.wsdl.parser.WSDLInternalizationLogic;
+
+public abstract class AbstractGenerateMojo extends AbstractMojo {
+    @Parameter(property="project", readonly=true, required=true)
+    private MavenProject project;
+
+    @Parameter(required=true)
+    private File[] wsdlFiles;
+
+    /**
+     * Corresponds to the {@code -p} option.
+     */
+    @Parameter
+    private String packageName;
+
+    @Parameter(required=true, defaultValue="${project.build.sourceEncoding}")
+    private String outputEncoding;
+
+    public final void execute() throws MojoExecutionException, MojoFailureException {
+        final Log log = getLog();
+        File outputDirectory = getOutputDirectory();
+        WsimportOptions options = new WsimportOptions();
+        options.defaultPackage = packageName;
+        for (File wsdlFile : wsdlFiles) {
+            options.addWSDL(wsdlFile);
+        }
+        // TODO: this is only suitable for test sources
+        // TODO: make generating the service class optional and skip this is no serivce is generated
+        options.wsdlLocation = wsdlFiles[0].toURI().toString();
+        options.sourceDir = outputDirectory;
+        ErrorReceiver errorReceiver = new ErrorReceiver() {
+            @Override
+            public void fatalError(SAXParseException exception) {
+                log.error(exception.getMessage());
+                log.debug(exception);
+            }
+            
+            @Override
+            public void error(SAXParseException exception) {
+                log.error(exception.getMessage());
+                log.debug(exception);
+            }
+            
+            @Override
+            public void warning(SAXParseException exception) {
+                log.warn(exception.getMessage());
+                log.debug(exception);
+            }
+            
+            @Override
+            public void info(SAXParseException exception) {
+                log.info(exception.getMessage());
+                log.debug(exception);
+            }
+
+            @Override
+            public void debug(SAXParseException exception) {
+                log.debug(exception);
+            }
+        };
+        MetadataFinder forest = new MetadataFinder(new WSDLInternalizationLogic(), options, errorReceiver);
+        forest.parseWSDL();
+        WSDLModeler wsdlModeler = new WSDLModeler(options, errorReceiver, forest);
+        Model wsdlModel = wsdlModeler.buildModel();
+        if (wsdlModel == null) {
+            throw new MojoExecutionException("Code generation failed");
+        }
+        SeiGenerator.generate(wsdlModel, options, errorReceiver);
+        ServiceGenerator.generate(wsdlModel, options, errorReceiver);
+        outputDirectory.mkdirs();
+        try {
+            CodeWriter codeWriter = new FileCodeWriter(outputDirectory, outputEncoding);
+            options.getCodeModel().build(codeWriter);
+        } catch (IOException ex) {
+            throw new MojoFailureException("Failed to write code", ex);
+        }
+        addSourceRoot(project, outputDirectory.getAbsolutePath());
+    }
+
+    protected abstract File getOutputDirectory();
+    protected abstract void addSourceRoot(MavenProject project, String path);
+}
---
+++ src/main/java/com/github/veithen/maven/wsimport/GenerateSourcesMojo.java
@@ -0,0 +1,43 @@
+/*-
+ * #%L
+ * wsimport-maven-plugin
+ * %%
+ * Copyright (C) 2018 Andreas Veithen
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.github.veithen.maven.wsimport;
+
+import java.io.File;
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+@Mojo(name="generate-sources", defaultPhase=LifecyclePhase.GENERATE_SOURCES, threadSafe=true)
+public class GenerateSourcesMojo extends AbstractGenerateMojo {
+    @Parameter(required=true, defaultValue="${project.build.directory}/generated-sources/wsimport")
+    private File outputDirectory;
+
+    @Override
+    protected File getOutputDirectory() {
+        return outputDirectory;
+    }
+
+    @Override
+    protected void addSourceRoot(MavenProject project, String path) {
+        project.addCompileSourceRoot(path);
+    }
+}
